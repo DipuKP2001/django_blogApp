3540912866e13176659cfeda850df3f05a24bb23
DipuKP2001 <dipukp602@gmail.com>
2020-07-08 23:18:40 +0530

    Basic blog app

---
+++ blog_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog_app/home.html', context)
+
+
+
+
+def about(request):
+    return render(request, 'blog_app/about.html', {'title':'About'})
